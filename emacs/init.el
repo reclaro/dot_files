a8ed543c4915bd9737ddd88366388b1a45a5508c
Andrea Rosa <andrea.rosa@gmail.com>
2020-04-22 23:27:03 +0100

    using use-package

--- emacs/init.el
+++ emacs/init.el
@@ -5,154 +5,19 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-(package-initialize) ;; You might already have this line
-
+(package-initialize)
+;; todo
+;; check smex vs amx. Installed smex but probably better to use it as smex-major-mode-commands which is smex only for the major mode but smex gives more hints.
+;; I think you need to try to use it
+;; try to better understand why projectile open project is enable only with one project open and not from the beginning. is it related to where that project file is?
+;; at the moment it is in the.emacs.d folder
 ;; load exec-path-from-shell for set the correct ENV path in MAC OS
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-env "GOPATH"))
 
-(add-to-list 'exec-path "/Users/androsa/code/go/bin")
-
-(add-hook 'markdown-mode-hook 'flyspell-mode)
-(require 'cl)
-
-(require 'company)
-(require 'company-lsp)
-(load-theme 'solarized-dark t)
-
-(set-default 'cursor-type 'box)
-
 ;; disable startup screen
 (setq inhibit-startup-message t)
-
-;; binding home and end to begging and end of line
-(global-set-key [home] 'move-beginning-of-line)
-(global-set-key [end] 'move-end-of-line)
-
-(tool-bar-mode -1)
-;; disable scrollbar
-(scroll-bar-mode -1)
-
-;;hopefully temporary: enable giude key
-(setq guide-key/guide-key-sequence 't)
-(guide-key-mode 1)
-
-;; enable automatic auto-revert
-(global-auto-revert-mode 1)
-
-;; show column number
-(setq column-number-mode t)
-
-;; Every time when the neotree window is opened, let it find current file and jump to node
-(setq new-smart-open t)
-
-(require 'ace-jump-mode)
-(define-key global-map (kbd "M-0") 'ace-jump-word-mode)
-; this is for not input the head char for word move, this can case more key press
-;(setq ace-jump-word-mode-use-query-char nil)
-(global-set-key (kbd "M-p") 'ace-window)
-
-;;(when (fboundp 'windmove-default-keybindings)
-;;    (windmove-default-keybindings))
-(global-set-key (kbd "s-<right>") 'windmove-right)
-(global-set-key (kbd "s-<left>") 'windmove-left)
-(global-set-key (kbd "s-<up>") 'windmove-up)
-(global-set-key (kbd "s-<down>") 'windmove-down)
-
-;; unset the list-buffers binding.
-(global-unset-key (kbd "C-x C-b"))
-
-(show-paren-mode 1)
-(set-face-foreground 'show-paren-match "white") ;;showing the matching paren in the specific color
-;;(set-face-attribute  'show-paren-match-face nil :weight 'extra-bold) ;;you can specify :height 130
-
-;; Hooks for clojure and clojure-script
-
-(add-hook 'clojure-mode-hook 'paredit-mode)
-;; Enable if you use clojure (add-hook 'clojure-mode-hook 'company-mode)
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'clojure-mode-hook 'whitespace-cleanup-mode)
-(add-hook 'clojurescript-mode-hook 'turn-on-eldoc-mode)
-;; enable it if you use clojurescript (add-hook 'clojurescript-mode-hook 'company-mode)
-(add-hook 'clojurescript-mode-hook 'whitespace-cleanup-mode)
-
-;;python ide when moved to emacs26 was returning an error
-;; TODO fix the eply-enable for python, or find something better
-;;(elpy-enable)
-
-;; Hooks for python
-(defun my-python-mode-hook ()
-  (local-set-key (kbd "M-,") 'pop-tag-mark)) ;; it doesn't work :(
-(add-hook 'python-mode-hook 'my-python-mode-hook)
-(add-hook 'python-mode-hook 'whitespace-cleanup-mode)
-
-
-
-;; use GNU find rather than pure elisp find
-;; https://gist.github.com/juergenhoetzel/8107a01039df08ea3f1c208494ddd7bf
-(defun go-packages-find ()
-  (sort
-   (delete-dups
-    (cl-mapcan (lambda (topdir)
-                 (let ((pkgdir (concat topdir "/pkg")))
-                   (mapcar (lambda (file)
-                             (let ((sub (substring file 0 -2)))
-                               (mapconcat #'identity (cdr (split-string sub "/")) "/")))
-                           (split-string (shell-command-to-string
-                                          (format "find \"%s\" -not -path \"%s/tool*\" -not -path \"%s/obj/*\" -name \"*.a\"  -printf \"%%P\\n\""
-                                                  pkgdir pkgdir pkgdir))))))
-               (go-root-and-paths)))
-   #'string<))
-(setq go-packages-function 'go-packages-find)
-
-;; Hooks for go
-(defun mrc-go-mode ()
-  (interactive)
-  (go-guru-hl-identifier-mode)
-  (go-eldoc-setup)
-  (flycheck-mode 1)
-  (lsp-deferred)
-  (set (make-local-variable 'company-backends) '(company-lsp company-capf company-files))
-  (company-mode 1)
-  (company-quickhelp-mode 1)
-  (local-set-key (kbd "C-c C-?") 'gofmt)
-  (local-set-key [remap xref-find-definitions] 'lsp-ui-peek-find-definitions)
-  (local-set-key [remap xref-find-references] 'lsp-ui-peek-find-references)
-  (local-set-key [remap xref-find-apropos] 'lsp-ui-peek-find-workspace-symbol)
-  (local-set-key (kbd "C-x 4 .") 'godef-jump-other-window)
-  (local-set-key (kbd "C-c .") 'godoc-at-point))
-
-(add-hook 'go-mode-hook #'mrc-go-mode)
-; Call Gofmt before saving
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-;;  disable snippet support. You can try to see if it is useful installing yasnippet
-(setq lsp-enable-snippet nil)
-
-(require 'go-guru)
-(go-guru-hl-identifier-mode)
-(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
-(add-hook 'go-mode-hook 'flyspell-prog-mode)
-
-;; Begin clojure flyckeck
-;; attempt to add flyckeck for clojure but got the error:
-;; 'flycheck-clojure-inject-jack-in-dependencies: Symbol's value as variable is void: cider-jack-in-dependencies-exclusions'
-;; to avoid this issue I think you need to run the flycheck-clojure-setup only after started a cider-jack-in. No it doesnt' work
-;(eval-after-load 'flycheck '(flycheck-clojure-setup))
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-
-; (eval-after-load 'flycheck
-;   '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
-;; end clojure flycheck
-
-;; General hooks
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-(add-hook 'org-mode-hook
-          (lambda()
-            (set-fill-column 110)))
-
-(add-hook 'python-mode-hook 'whitespace-cleanup-mode)
 ;; disable auto-save
 (setq auto-save-default nil)
 
@@ -161,117 +26,164 @@
 (setq mouse-wheel-progressive-speed 't) ;; don't accelerate scrolling
 (setq scroll-margin 1) ;; trigger the scrolling when I am on the last line
 
-;; show line number
-(global-linum-mode 1)
+;; Look and Feel
+(load-theme 'solarized-dark t)
+;;(load-theme 'spacemacs-dark t)
 
-;;Expanding region hitting the bound char
-;; c-= then use just = to expand, - to reduce 0 to reset and C-g to exit
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
+;; show column number along with line number in the status bar
+(setq column-number-mode t)
+;; disable bars
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(require 'linum-highlight-current-line-number)
 
-;;If enabled when a region is selected a new char delete the region
-(delete-selection-mode 1)
+;(display-line-numbers-mode) ;; available from emacs 26
+;; global  mapping
+(global-set-key (kbd "M-,") 'pop-tag-mark)
+(global-set-key [f8] 'neotree-toggle)
+(global-set-key [f6] 'show-file-name)
+;; misc
+;; When running ‘projectile-switch-project’ (C-c p p), ‘neotree’ will change root automatically.
+(setq projectile-switch-project-action 'neotree-projectile-action)
 
-;; to show file name when you press c-f6
+;;set backup folder and settings
+(setq
+   backup-by-copying t
+   backup-directory-alist
+    '(("." . "~/.emacsbackup"))
+   delete-old-versions t ; don't ask to delete excess backup versions
+   kept-new-versions 3
+   kept-old-versions 2
+   version-control t); use version number for backups
+
+;; to show file name
 (defun show-file-name ()
   "Show the full path file name in the minibuffer."
   (interactive)
   (message (buffer-file-name)))
-(global-set-key [C-f6] 'show-file-name)
 
-;; highlight tabs, spaces and empty line (at the beginning and end of file)
-(setq
-   whitespace-style '(face trailing empty)) ;; you can add tabs to highlight tabs as well
-(global-whitespace-mode 1)
+;; packages
+(require 'company-lsp)
+(push 'company-lsp company-backends)
 
-;; do not write tab
-(setq-default indent-tabs-mode nil)
+(require 'ag)
+;; auto-revert if the file is updated by an external program
+(global-auto-revert-mode 1)
 
-;; enable IDO InteractivelyDoThings
-(require 'ido)
-(ido-mode 1)
-;; need it if you get TooBig error  (setq ido-max-directory-size 100000)
-
-;;set backup folder and settings
-(setq
-   backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist
-    '(("." . "~/.emacsbackup"))    ; don't litter my fs tree
-   delete-old-versions t
-   kept-new-versions 3
-   kept-old-versions 2
-   version-control t)       ; use versioned backups
-
-;; enable rainbow
-(require 'rainbow-delimiters)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-
-;; easy way to highligt symbol under the curso and go throught the occurences
-(require 'highlight-symbol)
-(global-set-key [f5] 'highlight-symbol)
-(global-set-key [(control f5)] 'highlight-symbol-next)
-(global-set-key [(shift f5)] 'highlight-symbol-prev)
-(global-set-key [(meta f5)] 'highlight-symbol-query-replace)
+(use-package use-package-chords
+  :ensure t
+  :config (key-chord-mode 1))
 
 
-;;using the Figwheel REPL connected in emacs
-;; Loading cider is taking 75 seconds I don't need it now so commenting but
-;; it is important to understand why and re-enable it eventually.
-;;(require 'cider)
-;;(setq cider-cljs-lein-repl
-;;     "(do (require 'figwheel-sidecar.repl-api)
-;;         (figwheel-sidecar.repl-api/start-figwheel!)
-;;        (figwheel-sidecar.repl-api/cljs-repl))")
+(use-package frog-jump-buffer
+  :ensure t
+  :chords (("bn" . frog-jump-buffer-other-window)
+           ("bb" . frog-jump-buffer)))
 
-(tabbar-mode 't)
+(use-package ace-jump-mode
+  :ensure t
+  :config
+  (ace-jump-mode-enable-mark-sync) ; doesn't seem to work
+; if you want to highlight all word use this  (setq ace-jump-word-mode-use-query-char nil)
+  :bind
+  ("C-." . 'ace-jump-mode)
+  ("C-:" . 'ace-jump-mode-pop-mark))
+; that creates an autoload for ace-jump-mode command and defers loading until you use it
 
+(use-package whitespace
+  :ensure t
+  :config
+  (global-whitespace-mode 1)
+  (setq whitespace-style '(face trailing empty)))
 
-;; disable the lsp-ui-sideline
-(setq lsp-ui-sideline-enable nil)
+(use-package linum
+  :config
+  (global-linum-mode t)
+  (setq linum-format 'linum-highlight-current-line-number))
+
+(use-package which-key
+  :config
+  (which-key-mode 1)
+  (setq which-key-show-early-on-C-h t);Allow C-h to trigger which-key before it is done automatically
+  :init
+  (which-key-setup-side-window-right-bottom))
+
+(use-package projectile
+  :config
+  (projectile-mode t)
+  :bind
+  (:map projectile-mode-map
+	("C-c p" . projectile-command-map)))
+
+(use-package smartparens
+  :config
+  (smartparens-global-mode t)
+  (show-smartparens-global-mode t))
+
+(use-package ido ;there is an alternative called flx-ido
+  :config
+  (ido-mode 1)
+  (ido-grid-mode 1) ; you can use ido-vertical-mode but in the grid-mode you can fit more data
+  (setq ido-everywhere 1
+	ido-enable-flex-matching t
+        ido-grid-mode-padding " || "))
+
+(use-package lsp-mode
+   :config
+   (setq lsp-file-watch-threshold 3000) ; over the threshold it will ask for confirmation to watch files, default is set to 1000
+   :hook ((go-mode . lsp))
+   :commands lsp)
+
+ (use-package lsp-ui
+   :commands lsp-ui-mode
+   :bind
+   (:map lsp-ui-mode-map
+	 ("<f12>" . lsp-ui-peek-find-references))
+   )
+
+(use-package go-mode
+
+  :init
+  (setq company-idle-delay nil)
+  (setq lsp-ui-doc-enable 1)
+  (setq lsp-ui-doc-delay nil)
+  (setq lsp-ui-doc-use-childframe 1)
+  (setq lsp-ui-doc-position (quote top))
+  (setq lsp-ui-sideline-enable nil)
+  (setq lsp-ui-peek-enable 1)
+
+  :bind
+  (:map go-mode-map
+	("M-SPC" . company-indent-or-complete-common)
+	("C-c d" . lsp-ui-doc-show)
+	("C-c t" . 'go-test-current-test)
+	("C-c C-t" . 'go-test-current-file)
+	("M-p" . 'compile))
+
+  :config
+  (go-eldoc-setup) ; it provides eldoc for go
+  (use-package godoctor)
+  (company-mode 1)
+  (lsp-deferred) ; lsp will start only after the buffer is visible
+  (company-quickhelp-mode 1)
+  (flyspell-prog-mode)
+  (add-hook 'before-save-hook 'whitespace-cleanup)
+  (add-hook 'before-save-hook 'gofmt-before-save))
+;; flycheck mode is enabled by default and all customisation I tried get ignored :(
+
+(setenv "GOPATH" "/home/andrea/code/go")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(highlight-symbol-colors
-   (quote
-    ("VioletRed1" "red1" "dark blue" "green3" "dark cyan" "purple4" "forest green")))
- '(highlight-symbol-foreground-color "gray100")
- '(lsp-ui-sideline-delay 5.0)
- '(lsp-ui-sideline-ignore-duplicate nil)
- '(lsp-ui-sideline-show-code-actions nil)
- '(markdown-open-command "macdown")
  '(package-selected-packages
    (quote
-    (markdown-preview-eww markdown-mode+ markdown-mode go-snippets lsp-mode company-go magit zenburn-theme yaml-mode whitespace-cleanup-mode terraform-mode tabbar sublime-themes spaceline solarized-theme smart-mode-line-powerline-theme seti-theme rainbow-delimiters paredit neotree monokai-theme highlight-symbol guide-key groovy-mode gradle-mode go-playground go-imports go-eldoc flyspell-correct flycheck-pos-tip flycheck-clojure expand-region exec-path-from-shell esup elpy elixir-mode css-eldoc color-theme-solarized ag ace-window ace-jump-mode)))
- '(tabbar-background-color "#0A2933")
- '(tabbar-mode t nil (tabbar))
- '(tabbar-mwheel-mode t nil (tabbar))
- '(tabbar-separator (quote (" ⚡ " nil)))
- '(tabbar-use-images nil))
+    (use-package-chords key-chord frog-jump-buffer ace-jump ace-jump-mode expand-region smex ido-grid-mode ido-vertical-mode neotree which-key ag projectile godoctor smartparens spacemacs-theme solarized-theme solaire-mode lsp-ui flyspell-correct company-quickhelp company-lsp company go-scratch go-dlv go-eldoc go-playground go-guru flycheck lsp-mode go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "nil" :family "Menlo"))))
- '(linum ((t (:foreground "orange1" :weight light :height 0.9))))
- '(lsp-ui-doc-background ((t (:background "#272A36" :box nil))))
- '(lsp-ui-sideline-global ((t (:height 90))))
- '(rainbow-delimiters-depth-1-face ((t (:foreground "dark cyan"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "dark magenta"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "maroon1"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "tomato1"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "orange1"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "gray57"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "yellow1"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "green1"))))
- '(rainbow-delimiters-depth-9-face ((t (:foreground "OrangeRed2"))))
- '(tabbar-button ((t (:inherit tabbar-default))))
- '(tabbar-default ((t (:inherit variable-pitch :foreground "gray50" :height 0.9))))
- '(tabbar-highlight ((t (:underline t))))
- '(tabbar-selected ((t (:inherit tabbar-default :foreground "orange1"))))
- '(tabbar-separator ((t (:inherit tabbar-default))))
- '(tabbar-unselected ((t (:inherit tabbar-default)))))
-(put 'upcase-region 'disabled nil)
+ )
