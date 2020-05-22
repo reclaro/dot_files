06e6c52623d75c1d184aa95893ff92ad52f8837b
Andrea Rosa <andrea.rosa@oracle.com>
2020-05-22 11:58:23 +0100

    Integration with appointment

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+'("melpa" . "http://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -156,11 +156,12 @@
 
   :config
   (setq org-startup-indented t)
-  (setq org-catch-invisible-edits 'show-and-error)
+  (setq org-catch-invisible-edits 'error)
   (setq org-hide-emphasis-markers t)
   (setq org-M-RET-may-split-line (quote ((default)))) ;; M-RET in the middle of a line doesn't break the line
   (setq org-log-done 'time) ; set this per file as well
   (setq org-treat-S-cursor-todo-selection-as-state-change nil)
+  ;(setq org-agenda-files (directory-files-recursively "~/Dropbox/org/" "\.org$"))
   (setq org-agenda-files (directory-files-recursively "~/Dropbox/org/" "\.txt$"))
   (setq org-time-stamp-custom-formats (quote ("<%d/%m/%y %a>" . "<%d/%m/%y %a %H:%M>")))
   (setq org-babel-load-languages
@@ -180,14 +181,43 @@
    (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
    (add-hook 'before-save-hook 'whitespace-cleanup))
 
+;; configuring integration with apt
+(require 'appt)
+(setq appt-time-msg-list nil)    ;; clear existing appt list
+(setq appt-display-interval '5) ;; warn every 10 minutes from t - appt-message-warning-time
+(setq
+  appt-message-warning-time '15  ;; send first warning 10 minutes before appointment
+  ;appt-display-mode-line nil     ;; don't show in the modeline
+  appt-display-format 'window)   ;; pass warnings to the designated window function
+(appt-activate 1)                ;; activate appointment notification
+(display-time)                   ;; activate time display
 
+
+(org-agenda-to-appt)             ;; generate the appt list from org agenda files on emacs launch
+(run-at-time "06:30" 300 'org-agenda-to-appt)           ;; update appt list hourly
+(add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt) ;; update appt list on agenda view
+
+;; set up the call to terminal-notifier
+(defvar my-notifier-path
+  "terminal-notifier")
+(defun my-appt-send-notification (title msg)
+  (shell-command (concat my-notifier-path " -message " msg " -title " title " -sound default" )))
+
+;; designate the window function for my-appt-send-notification
+(defun my-appt-display (min-to-app new-time msg)
+  (my-appt-send-notification
+   (if (string= min-to-app "0")
+	(format "'Appointment NOW'")
+	(format "'Appointment in %s minutes'" min-to-app))
+    ;(format "'Appointment in %s minutes'" min-to-app)    ;; passed to -title in terminal-notifier call
+    (format "'%s'" msg)))                                ;; passed to -message in terminal-notifier call
+(setq appt-disp-window-function (function my-appt-display))
 
 (use-package all-the-icons)
 (use-package anzu
   :ensure t
   :config
   (global-anzu-mode 1))
-
 
 (use-package flyspell
   :ensure t
@@ -274,6 +304,17 @@
   :config
   (smartparens-global-mode t)
   (show-smartparens-global-mode t))
+  ;; :bind
+  ;; (:map smartparens-mode-map
+  ;; 	("M-<right>" . sp-forward-slurp-sexp) ; slurp move parenthesis
+  ;; 	("M-<left>" . sp-backward-slurp-sexp)
+  ;; 	("s-<right>" . sp-forward-barf-sexp) ; barf move word
+  ;; 	("s-<left>" . sp-backward-barf-sexp)))
+
+(define-key smartparens-mode-map (kbd "s-<right>") 'sp-forward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "s-<left>") 'sp-backward-slurp-sexp)
+(define-key smartparens-mode-map (kbd "C-s-<right>") 'sp-forward-barf-sexp)
+(define-key smartparens-mode-map (kbd "C-s-<left>") 'sp-backward-barf-sexp)
 
 (use-package ido-grid-mode
   :ensure t)
@@ -285,7 +326,7 @@
   (ido-grid-mode 1) ; you can use ido-vertical-mode but in the grid-mode you can fit more data
   (setq ido-everywhere 1
 	ido-enable-flex-matching t
-        ido-grid-mode-padding " || "))
+	ido-grid-mode-padding " || "))
 
 (use-package lsp-mode
    :ensure t
@@ -357,9 +398,6 @@
     ("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default)))
  '(gnutls-algorithm-priority "normal:-vers-tls1.3")
  '(markdown-make-gfm-checkboxes-buttons nil)
- '(org-agenda-files
-   (quote
-    ("/Users/androsa/Dropbox/org/billbook.txt" "/Users/androsa/Dropbox/org/emacstodo.txt" "/Users/androsa/Dropbox/org/todo.txt")))
  '(package-selected-packages
    (quote
     (zop-to-char elpy doom-themes kaolin-themes treemacs-icons-dired treemacs-projectile treemacs org-superstar paradox spaceline-all-the-icons anzu spaceline all-the-icons-gnus yaml-mode load-theme-buffer-local color-theme-buffer-local magit flymake exec-path-from-shell let-alist use-package-chords key-chord frog-jump-buffer ace-jump ace-jump-mode expand-region smex ido-grid-mode ido-vertical-mode which-key ag projectile godoctor smartparens spacemacs-theme solarized-theme solaire-mode lsp-ui flyspell-correct company-quickhelp company go-scratch go-dlv go-eldoc go-playground go-guru flycheck go-mode)))
